867ee453a662924b526b5375caae576f977f2c87
generaldenmark <asger@weirsoe.dk>
2018-09-11 15:38:49 +0200

    Final ish

--- main.py
+++ main.py
@@ -19,36 +19,69 @@ def main(job_title, company, username=None, password=None, url=None, lan = None)
             exit(1)
         synk = True
 
+
     while lan is not 'DA' and lan is not 'EN':
         lan = decide_lan()
 
+    print(f'\nCreating folder for: {company}\n'
+          f'With jobtitle {job_title}')
+    print(f'Synk is ' + 'enabled' if synk else 'disenabled')
+    print(f'Language is {lan}')
+
     source = os.path.join('src', 'Danish' if lan is 'DA' else 'English')
-    destination = os.path.join('ansoegnings_superfolder', company)
+    print(f'get original path: {source}')
+    destination = os.path.join('ansøgnings_superfolder', company.replace('/', '').replace(' ', ''))
+    print(f'destination is: {destination}')
 
     if synk:
         latex_latest = get_latest(source)
     else:
         latex_latest = get_local_latest(os.path.join(source, 'latex'))
+    print(f'Latest CV latex version: {latex_latest}')
 
     application(source, job_title, company, destination)
 
     copy(latex_latest, os.path.join(destination, 'tmp', 'latex'))
     preamble(os.path.join(destination,'tmp','latex','preamble.tex'), company)
+    compile_latex(os.path.join(destination,'tmp','latex'))
+
+    clean_up(os.path.join(destination, 'tmp'))
+
+    copy_last_parts(destination)
+
+def clean_up(path):
+    shutil.rmtree(path)
 
 
+def copy_last_parts(destination):
+    src = os.path.abspath('src/pp_eb')
+    src_files = os.listdir(src)
+    for filenames in src_files:
+        full_filename = os.path.join(src, filenames)
+        if (os.path.isfile(full_filename)):
+            shutil.copy(full_filename, destination)
+
+def compile_latex(path):
+    total_path = os.path.abspath(path)
+
+    bash = 'xelatex CV.tex'
+    subprocess.call(bash.split(), cwd=path, stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+    try:
+        move_command = 'cp CV.pdf ../../Curriculum_Vitae.pdf'
+        subprocess.Popen(move_command.split(), cwd=path)
+    except Exception as e:
+        print(e)
 
 def preamble(source, company):
     with open(source, 'r') as f:
         filedate = f.read().replace('COMPANYPLACEHOLDER', company)
 
-    with open(source, 'w') as f:
+    with open(source, 'w')as f:
         f.write(filedate)
 
 
 def copy(source, destination):
     try:
-        print(source)
-        print(destination)
         shutil.copytree(source, destination)
     except OSError as e:
         # If the error was caused because the source wasn't a directory
@@ -82,21 +115,22 @@ def get_latest(path):
 
 
 def application(source, jobtitle, company, destination):
-
+    print('Application commence')
     try:
         os.makedirs(os.path.join(destination, 'tmp'))
     except OSError as e:
         if e.errno != errno.EEXIST:
             raise
+        print('Folder already located!')
 
     source = os.path.join(source, 'Application.rtf')
-    new_source = os.path.join(destination, 'tmp', 'application.rtf')
-
+    new_source = os.path.join(destination, 'tmp')
     with open(source, 'r') as input:
-        with open(new_source, 'w') as output:
+        with open(new_source + '/application.rtf', 'w') as output:
             output.write(input.read().replace('JOBTITLE', jobtitle).replace('COMPANY', company))
+    print(f'Edit + copy success.')
 
-    convert_to_pdf(new_source, os.path.join(destination, 'folder'))
+    convert_to_pdf(new_source, destination)
 
 
 def decide_lan():
@@ -114,9 +148,13 @@ def convert_to_pdf(source, destination):
     except OSError as e:
         if e.errno != errno.EEXIST:
             raise
-    bash = f'libreoffice --headless --invisible --norestore --convert-to pdf --outdir {os.path.abspath(destination)} {os.path.abspath(source)}'
+        print('Destination located!')
+    bash = f'libreoffice --headless --invisible --norestore --convert-to pdf --outdir {os.path.abspath(destination)} application.rtf'
+    try:
+        subprocess.Popen(bash.split(), cwd=source)
+    except Exception as e:
+        print('Could not convert to .pdf.\n' + str(e))
 
-    subprocess.check_output(bash.split())
 
 ap = argparse.ArgumentParser(description="Application Generator", epilog="This requires that you have some pretty presice instructions. See sourcecode..")
 
